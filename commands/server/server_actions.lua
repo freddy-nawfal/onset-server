f2216a1786863a4950d3b8062628b46bd233f27d
Freddy <nawfal.freddy@gmail.com>
2019-12-17 13:02:46 +0100

    display inventory

--- commands/server/server_actions.lua
+++ commands/server/server_actions.lua
@@ -5,7 +5,7 @@ AddRemoteEvent("commands:inventory", function(player)
 	local p = otherplayer or player
 
 	print("items: ")
-	print(json_encode(horizon.GetPlayerData(player).items))
+	print(json.encode(horizon.GetPlayerData(player).items))
 
 	p = tonumber(p)
 
